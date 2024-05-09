ab014e49e6606703475b830c7b27fae8614886eb
MingQce <1420395899@qq.com>
2024-05-09 19:28:44 +0800

    sentinel 热点参数限流

--- borrow-service/src/main/java/com/test/controller/BorrowController.java
+++ borrow-service/src/main/java/com/test/controller/BorrowController.java
@@ -7,6 +7,7 @@ import com.test.entity.UserBorrowDetail;
 import com.test.service.BorrowService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -38,20 +39,10 @@ public class BorrowController {
     }
 
     @RequestMapping("/test")
-    @SentinelResource(value = "test",
-            fallback = "except",    //fallback指定出现异常时的替代方案
-            blockHandler = "block",
-            exceptionsToIgnore = IOException.class)  //忽略那些异常，也就是说这些异常出现时不使用替代方案
-    String test(){
-        throw new RuntimeException("HelloWorld！");
-    }
-
-    //替代方法必须和原方法返回值和参数一致，最后可以添加一个Throwable作为参数接受异常
-    String except(Throwable t){
-        return t.getMessage();
-    }
-
-    String block(BlockException e){
-        return "被限流了";
+    @SentinelResource("test")   //注意这里需要添加@SentinelResource才可以，用户资源名称就使用这里定义的资源名称
+    String findUserBorrows2(@RequestParam(value = "a", required = false) String a,
+                            @RequestParam(value = "b", required = false) String b,
+                            @RequestParam(value = "c",required = false) String c) {
+        return "请求成功！a = "+a+", b = "+b+", c = "+c;
     }
 }
